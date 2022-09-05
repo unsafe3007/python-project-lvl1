0079720e84eb86cde73c3200b27d17fe89a4cfae
Pavel Smushko <unsafe3007@gmail.com>
2022-09-05 10:54:09 +0200

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
 from brain_games.cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!!!')
@@ -8,7 +8,5 @@ def main():
     welcome_user()
 
 
-
 if __name__ == '__main__':
     main()
-
