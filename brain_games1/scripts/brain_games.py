3b4fd3b77b205cd98a34140ab65ab479f275ade0
Pavel Smushko <unsafe3007@gmail.com>
2022-09-06 11:21:49 +0200

    reinstall dist

--- brain_games1/scripts/brain_games.py
+++ brain_games1/scripts/brain_games.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from brain_games1.cli import welcome_user
-from brain_games1.scripts.brain_even import is_even
 
 
 def main():
@@ -8,7 +7,6 @@ def main():
     print('Welcome to the Brain Games!!!')
 
     name = welcome_user()
-    is_even(name)
 
 
 if __name__ == '__main__':
