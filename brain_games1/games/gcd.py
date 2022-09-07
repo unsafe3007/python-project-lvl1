e891ff4cc883b9e9443e10bf5bcc0dac3917fe93
Pavel Smushko <unsafe3007@gmail.com>
2022-09-07 12:19:58 +0200

    Step 7, add brain-gcd

---
+++ brain_games1/games/gcd.py
@@ -0,0 +1,41 @@
+from random import randint
+
+
+def get_gcd(num_1, num_2):
+    while num_1 != 0 and num_2 != 0:
+        if num_1 > num_2:
+            num_1 = num_1 % num_2
+        else:
+            num_2 = num_2 % num_1
+
+    gcd = num_1 + num_2
+
+    return gcd
+
+
+def get_answer(name):
+    print('Find the greatest common divisor of given numbers.')
+
+    count = 0
+
+    while count < 3:
+        num_1 = randint(1, 100)
+        num_2 = randint(1, 100)
+
+        gcd = get_gcd(num_1, num_2)
+
+        print('Question:', num_1, num_2)
+
+        answer = int(input('Your answer: '))
+
+        if answer == gcd:
+            print('Correct!')
+
+            count += 1
+        else:
+            print(f'{answer} is wrong answer ;(. '
+                  f"Correct answer was {gcd}. Let's try again {name}! ")
+            break
+
+    if count == 3:
+        print(f'Congratulations {name}!!!')
---
+++ brain_games1/scripts/brain_gcd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from brain_games1.cli import welcome_user
+from brain_games1.games.gcd import get_answer
+
+
+def main():
+
+    print('Welcome to the Brain Games!!!')
+
+    name = welcome_user()
+    get_answer(name)
+
+
+if __name__ == '__main__':
+    main()
