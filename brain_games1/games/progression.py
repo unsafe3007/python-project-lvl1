b72a90d29964c0e7583a403a5bed7fcebef9cacf
Pavel Smushko <unsafe3007@gmail.com>
2022-09-09 11:38:56 +0200

    refactored progression.py

--- brain_games1/games/progression.py
+++ brain_games1/games/progression.py
@@ -1,25 +1,32 @@
 from random import randint, choice
 
 
+def generate_progression():
+    numbers = []
+    first_num = randint(1, 10)
+    step = randint(2, 10)
+    lenght = randint(6, 10)
+    last_num = first_num + step * lenght
+
+    for i in range(first_num, last_num, step):
+        numbers.append(i)
+
+    correct_answer = choice(numbers)
+
+    num_x = numbers.index(correct_answer)
+
+    numbers[num_x] = ".."
+    progression = " ".join(map(str, numbers))
+
+    return correct_answer, progression
+
+
 def get_progression(name):
     print('What number is missing in the progression?')
     total = 0
     while total < 3:
-        numbers = []
-        first_num = randint(1, 10)
-        step = randint(2, 10)
-        lenght = randint(6, 10)
-        last_num = first_num + step * lenght
+        correct_answer, progression = generate_progression()
 
-        for i in range(first_num, last_num, step):
-            numbers.append(i)
-
-        correct_answer = choice(numbers)
-
-        num_x = numbers.index(correct_answer)
-
-        numbers[num_x] = ".."
-        progression = " ".join(map(str, numbers))
         print("Question:", progression)
         answer = int(input('Your answer: '))
         if answer == correct_answer:
